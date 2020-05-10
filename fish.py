30702855d93cba0d0d5cf7ce93cee3b2ceefaac9
aliceconcordel <alice.concordel@gmail.com>
2020-05-10 11:46:12 +0200

    ACO: All three rules implemented, not yet fully tested.

--- fish.py
+++ fish.py
@@ -1,4 +1,6 @@
 import math
+from random import randint
+from typing import List
 
 from PyQt5.QtCore import QPointF, QRectF
 from PyQt5.QtGui import QPainterPath, QPolygonF, QPainter
@@ -6,14 +8,14 @@ from PyQt5.QtWidgets import QGraphicsItem
 
 
 class Fish(QGraphicsItem):
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, speed, color, name):
         super().__init__(None)
         self._x = x
         self._y = y
         self._theta = 0
         self.setPos(self._x, self._y)
         self.setRotation(self._theta)
-        self._speed = 3
+        self._speed = speed
         self._color = color
 
         points = [QPointF(10, 0), QPointF(8, -3),QPointF(5, -5),QPointF(-10, 0), QPointF(5, 5), QPointF(8, 3)]
@@ -25,13 +27,20 @@ class Fish(QGraphicsItem):
         self._swim_counter = 0
         self._theta_swim = 0
 
+        self._name = name
+        self._cohesive_r = 20
+        self._repulsive_r = 20
+
     def paint(self, painter, options, widget=None):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setBrush(self._color)
         painter.drawPath(self._path)
+        painter.rotate(-self._theta - self._theta_swim)
+        painter.drawText(10, -10, self._name)
 
     def boundingRect(self):
-        return QRectF(-10, -10, 20, 20)
+        # return QRectF(-10, -10, 20, 20)
+        return QRectF(-100, -100, 200, 200)
 
     def move(self):
         self.swim()
@@ -53,6 +62,31 @@ class Fish(QGraphicsItem):
 
     def set_theta(self, theta):
         self._theta = theta
+
+    def nearby(self, other: "Fish"):
+        distance = math.sqrt((self._x-other._x)**2 +(self._y-other._y)**2)
+        return distance < self._cohesive_r, distance < self._repulsive_r
+
+    def cohesive(self, neighbours: List["Fish"]):
+        thetas = []
+        speeds = []
+        for i in neighbours:
+            thetas.append(i._theta)
+            speeds.append(i._speed)
+        self._theta = self._theta * 0.8 + 0.2 * sum(thetas)/len(thetas)
+        self._speed = self._speed * 0.8 + 0.2 * sum(speeds)/len(speeds)
+
+    def repulsive(self, crowders: List["Fish"]):
+        for i in crowders:
+            repulsion = [-(i._x - self._x), -(i._y - self._y)]
+            speed_vector = [self._speed*math.cos(self._theta), self._speed*math.sin(self._theta)]
+            new_speed_vector = [0.8*speed_vector[0] + 0.2*repulsion[0], 0.8*speed_vector[1] + 0.2*repulsion[1]]
+            self._speed = math.hypot(new_speed_vector[0], new_speed_vector[1])
+            self._theta = math.atan(new_speed_vector[1]/new_speed_vector[0])
+
+    def random(self):
+        self._theta += 0.2 * (randint(0, 100)/50 - 1) * 90
+        self._speed += 0.2 * self._speed * ((randint(0, 100)/50) - 1)
 
     @property
     def theta(self):
--- fish_manager.py
+++ fish_manager.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QColor
 
 from fish import Fish
+from utils import random_name
 
 TICK = 40
 
@@ -20,12 +21,23 @@ class FishManager:
 
         for i in range(1, 100):
             color = QColor(randint(0,255), randint(0,255), randint(0,255))
-            fish = Fish(randint(0, scene.width()), randint(0, scene.height()), color)
+            fish = Fish(randint(0, scene.width()), randint(0, scene.height()), randint(1, 5), color, name=random_name())
             self.add_fish(fish)
             fish.set_theta(randint(0, 360))
 
     def update(self):
         for fish in self._fishes:
+            neighbours = []
+            crowders = []
+            for i in self._fishes:
+                close, too_close = fish.nearby(i)
+                if close:
+                    neighbours.append(i)
+                if too_close:
+                    crowders.append(i)
+            fish.cohesive(neighbours)
+            # fish.repulsive(crowders)
+            fish.random()
             fish.move()
 
     def add_fish(self, fish):
